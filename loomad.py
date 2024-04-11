6536209b541e957db10abec033ff111efa003562
HenriDaGoat <165260735+HenriDaGoat@users.noreply.github.com>
2024-04-11 12:49:44 +0300

    Karakter1

--- loomad.py
+++ loomad.py
@@ -1,2 +1,11 @@
 #OLIVER OSTRAT, KEIRO TABUR, TANEL NÕMM
+class Karakter:
+    def _init_(self, nimi, elud, tegevus, kaotaElusi, lisaelusi):
+        self.nimi = nimi
+        self.elud = 100
+        self.tugevus = 20
+        self.kaotaElusi = 30
+        self.lisaelusi = 40
+         
+
 
